4463d0685618f1fb3439421bb1a9282adb7fd290
Erika <Erika1111@yandex.ru>
2023-01-10 14:23:01 +0300

    homework9

---
+++ Atask1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+Console.WriteLine("Введите число N: ");
+int num = int.Parse(Console.ReadLine());
+
+string SumNum(int a)
+{
+    if (a == 1)
+    return a.ToString();
+    else
+    {
+        return SumNum(a - 1) + " , " + a;
+    }
+}
+Console.WriteLine(SumNum(num));
